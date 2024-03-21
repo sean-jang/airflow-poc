185ed26d369a2872a240d90e9210a6506cf5f65e
sean-jang <summertrees87@gmail.com>
2024-03-21 15:25:12 +0900

    Modify S3 custom hook with aws provider

--- dags/custom/s3_hook.py
+++ dags/custom/s3_hook.py
@@ -6,8 +6,72 @@ import pandas as pd
 from airflow.exceptions import AirflowException
 from airflow.macros import ds_add
 from airflow.hooks.base import BaseHook
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
+class CustomS3Hook(S3Hook):
+    DEFAULT_REGION = 'ap-northeast-2'
+
+    def __init__(self,conn_id='aws_access'):
+        super().__init__(conn_id)
+    
+    def extract_from_s3(self,
+                        execution_date: str,
+                        key: str,
+                        bucket='amplitude-export-test-116807',
+                        **kwargs) -> pd.DataFrame  :
+        
+        # next_ds = ds_add(execution_date, 1)
+        next_ds = '2024-02-20'
+        response = self.get_key(
+            key=f'airflow-test/bronze/goodoc-rds/{key}/date_id={next_ds}/0.csv',
+            bucket_name=bucket
+        )
+        df = pd.read_csv(response.get()["Body"])
+        
+        if df.empty:
+            raise AirflowException(f"Unsuccessful S3 get_object {key} response")
+        else:
+            return df
+        
+    
+    def load_dataframe_to_s3(self,
+                             df: pd.DataFrame,
+                             execution_date: str,
+                             key: str,
+                             bucket='amplitude-export-test-116807',
+                             **kwargs):
+        
+        # next_ds = ds_add(execution_date, 1)
+        next_ds = '2024-02-20'
+
+        with io.BytesIO() as csv_buffer:
+            df.to_csv(csv_buffer, index=False)
+            self.load_bytes(
+                key=f'gold/daily_key_metrics/date_id={next_ds}/{key}_summary.csv',
+                bucket_name=bucket,
+                bytes_data=csv_buffer.getvalue(),
+                replace=True
+            )
+    
+    def get_objects_list(self, 
+                         execution_date, 
+                         prefix, 
+                         bucket='amplitude-export-test-116807', 
+                         **kwargs) -> list :
+        
+        # next_ds = ds_add(execution_date, 1)
+        next_ds = '2024-02-20'
+        keys = self.list_keys(
+            bucket_name=bucket,
+            prefix=prefix
+        )
+        
+        return keys
+
+
+
+## Initial
 class S3Hook(BaseHook):
     DEFAULT_CONN = 's3'
     DEFAULT_REGION = 'ap-northeast-2'
